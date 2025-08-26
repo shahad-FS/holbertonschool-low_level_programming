d147d4dfd64683669ba7d3ccc9a23767a841e1b6
shahad-FS <alsaneea.s@gmail.com>
2025-08-26 10:53:31 +0000

    docs : Create README.md file

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; word[i] != '\0'; i++)
+	{
+	    _putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
