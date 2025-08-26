a1e615327562e2246375aa4eb8af2b4a09355682
shahad-FS <alsaneea.s@gmail.com>
2025-08-26 11:57:11 +0000

    changes

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -13,11 +13,6 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	_putchar('\n');
-}
 
-int main(void)
-{
-	print_alphabet();
-	return(0);
+	_putchar('\n');
 }
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
