6ae0626d20882dcec7dc24ad5d3418d5f265c4c5
shahad-FS <alsaneea.s@gmail.com>
2025-08-26 13:05:18 +0000

    create function

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, from 0
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0' + prod);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (prod < 10)
+					_putchar(' ');
+				else
+					_putchar('0' + prod / 10);
+
+				_putchar('0' + prod % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
