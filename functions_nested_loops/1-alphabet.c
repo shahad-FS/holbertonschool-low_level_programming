20f96e876dd0066cb7c6285f1accef6496ea5933
shahad-FS <alsaneea.s@gmail.com>
2025-08-26 11:47:20 +0000

    alphabitcs

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description:
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}
