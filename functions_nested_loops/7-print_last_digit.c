476eeb062cde1728f5bc1a21379c92f900b98c14
shahad-FS <alsaneea.s@gmail.com>
2025-08-26 12:56:31 +0000

    create function

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: The number to check
+ *
+ * Return: Value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+		last = -last;
+
+	_putchar('0' + last);
+
+	return (last);
+}
