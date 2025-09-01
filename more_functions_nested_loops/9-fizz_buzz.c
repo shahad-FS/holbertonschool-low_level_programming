babcd3782bef02e35d124e123e729270857df04a
shahad-FS <alsaneea.s@gmail.com>
2025-09-01 05:45:28 +0000

    update

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,20 +7,15 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3) == 0 && (num % 5) == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if ((num % 3) == 0)
-		{
+			printf("Fizz");
+		else if ((num % 5) == 0)
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", num);
-		}
-		if (num == 100)
-			continue;
-		printf(" ");
+		if (num != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
