ba4452251aa90bbecee402da9a59d6c408d68da8
shahad-FS <alsaneea.s@gmail.com>
2025-09-01 03:02:48 +0000

    Create function _isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,15 @@
+/*
+ * File: 0-isupper.c
+ *
+ */
+
 #include "main.h"
 
-/*
- * _is upper - Check if a character is uppercase.
- * Return: 1 if is uppercase, 0 otherwies.
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
