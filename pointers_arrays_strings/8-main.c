ec9259ad792257d7890ecf6faca631cd08fd0021
shahad-FS <alsaneea.s@gmail.com>
2025-09-02 05:18:06 +0000

    Create function print array

---
+++ pointers_arrays_strings/8-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int array[5];
+
+	array[0] = 55;
+	array[1] = 25;
+	array[2] = -64;
+	array[3] = 745;
+	array[4] = 1250;
+	print_array(array, 5);
+	return (0);
+}
