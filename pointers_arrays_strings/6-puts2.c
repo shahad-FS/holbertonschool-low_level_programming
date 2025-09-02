ccb99f238ba2ba092be89b467627c32536f2de45
shahad-FS <alsaneea.s@gmail.com>
2025-09-02 04:57:31 +0000

    Create function puts2 that print every other characters in the string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - print one char out of two of a string 
- * @str: pointer to the string 
+ * puts2 - print one char out of two of a string
+ * @str: pointer to the string
  *
- * Return : void 
+ * Return : void
  */
 void puts2(char *str)
 {
-	int index = 0;
+	int index = 0, len = 0;
 
-	while (str[index] != '\0')
+	while (str[index++])
+	{
+		len++;
+	}
+	for (index = 0; index < len; index += 2)
 	{
 		_putchar(str[index]);
-		index += 2;
 	}
 	_putchar('\n');
 }
