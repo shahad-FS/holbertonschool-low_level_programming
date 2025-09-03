8c6795f1d3f9d50c4ac2813eafa2411e122da6bf
shahad-FS <alsaneea.s@gmail.com>
2025-09-03 02:58:30 +0000

    updates to follow betty style

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates the string pointed to by @src, including the terminating
+ * _strcat - Concatenates the string pointed to by @src,
+	 including the terminating
  * null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
