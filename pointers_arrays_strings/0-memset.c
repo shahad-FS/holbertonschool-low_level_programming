0d60e8c4c4ae39d8b7b191205a72cffe83220c29
shahad-FS <alsaneea.s@gmail.com>
2025-09-09 16:21:49 +0000

    Create memset function

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * memset - fills the first n bytes of the  memory area
+ * _memset - fills the first n bytes of the  memory area
  * pointed to by @s with constant byte @c
  * @s: A pointer to the memory area to be filled
- * @c: The character to fill the memory area with
+ * @b: constant byte
  * @n: The number of bytes to be filled 
  *
  * Return: A pointer to the filled memory area @s 
