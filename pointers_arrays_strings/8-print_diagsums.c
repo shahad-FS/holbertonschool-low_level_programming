124d9743afae906662cd5259a67f6fffe003188d
shahad-FS <alsaneea.s@gmail.com>
2025-09-09 17:32:09 +0000

    updates

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	unsigned int sum1 = 0, sum2 = 0;
+	long int sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		sum1 += *(a + i * size + i);
 		sum2 += *(a + i * size + (size - 1 - i));
 	}
-	printf("%u, %u\n", sum1, sum2);
+	printf("%ld, %ld\n", sum1, sum2);
 }
