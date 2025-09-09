0212300b1fd390daf3124071cf61eb11e3af5700
shahad-FS <alsaneea.s@gmail.com>
2025-09-09 16:18:10 +0000

    Create memset function

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * memset - fills the first n bytes of the  memory area
@@ -9,13 +9,12 @@
  *
  * Return: A pointer to the filled memory area @s 
  */
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-		memory[index] = value;
+		s[index] = b;
 
-	return (memory);
+	return (s);
 }
