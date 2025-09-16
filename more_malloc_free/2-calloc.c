f3207473f1835a46dee46e50fbe3039a02518f77
shahad-FS <alsaneea.s@gmail.com>
2025-09-16 11:11:26 +0000

    update code style to follow betty style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * _calloc - Allocate memory for an array of a certain number 
+ * _calloc - Allocate memory for an array of a certain number
  * of element each of an inputted byte size
- * @numemb: The number of elements
+ * @nmemb: The number of elements
  * @size: The byte size of each array element
  *
  * Return: If nmemb = 0, size = 0, of the function fials - NULL
- * otherwise - a pointer ti the allocated memory 
+ * otherwise - a pointer ti the allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 
 	mem = malloc(size * nmemb);
-	
+
 	if (mem == NULL)
 	{
 		return (NULL);
