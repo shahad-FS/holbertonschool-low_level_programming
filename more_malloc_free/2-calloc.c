58428dc8551b952a8b5d64b08987f235d6cbd8e1
shahad-FS <alsaneea.s@gmail.com>
2025-09-16 10:59:23 +0000

    Create calloc function

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory for an array of a certain number 
+ * of element each of an inputted byte size
+ * @numemb: The number of elements
+ * @size: The byte size of each array element
+ *
+ * Return: If nmemb = 0, size = 0, of the function fials - NULL
+ * otherwise - a pointer ti the allocated memory 
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+	char *filler;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = malloc(size * nmemb);
+	
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+	filler = mem;
+
+	for (index = 0; index < (size * nmemb); index++)
+	{
+		filler[index] = '\0';
+	}
+	return (mem);
+}
