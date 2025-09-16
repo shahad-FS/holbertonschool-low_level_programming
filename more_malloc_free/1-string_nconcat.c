ba33aaa0bfcf2c21024111497422f557834babe3
shahad-FS <alsaneea.s@gmail.com>
2025-09-16 04:31:44 +0000

    update code

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	len = 0;
 
-	for (index = 0; s2[index]; index++)
+	for (index = 0; s1[index]; index++)
+	{
+		concat[len++] = s1[index];
+	}
+	for (index = 0; s2[index] && index < n; index++)
 	{
 		concat[len++] = s2[index];
 	}
