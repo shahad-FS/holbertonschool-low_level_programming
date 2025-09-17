a13574647aa8d96a8bec5f9f905ca231c30327af
shahad-FS <alsaneea.s@gmail.com>
2025-09-17 03:34:58 +0000

    Create init_dog function

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -24,5 +24,5 @@ struct dog
  */
 typedef struct dog dog_t;
 
-oid init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
