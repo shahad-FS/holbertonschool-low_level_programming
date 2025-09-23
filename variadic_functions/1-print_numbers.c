57c87d3b52dbf79dc8ecb1be87a7591ee706e615
shahad-FS <alsaneea.s@gmail.com>
2025-09-23 15:48:18 +0000

    Update the code to follow betty style

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/*
+/**
  * print_numbers - Prints numbers, followed by a new line.
- * @separator: The string to be printed between numbers
- * @n: The numbers of integers passed to the function
- * @...: A variable number of numbers to be printed
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
