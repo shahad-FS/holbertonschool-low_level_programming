e1baecfd9160be482575a27dbe8c509a5c363641
shahad-FS <alsaneea.s@gmail.com>
2025-10-07 16:38:41 +0000

    update the code

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,3 +22,5 @@ typedef struct list_s
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
+#endif
