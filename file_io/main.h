cb38489c84652eeb05db2b74628c7efad5d58b8a
shahad-FS <alsaneea.s@gmail.com>
2025-10-14 13:53:58 +0000

    Create directory for file i/o functions

--- file_io/main.h
+++ file_io/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * Desc: Header file containing prototypes for all functions
- * written in file_io directory
- */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
