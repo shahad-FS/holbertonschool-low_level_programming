6679b8613f8c11d20ea0227d0cb286eed4787b3d
shahad-FS <alsaneea.s@gmail.com>
2025-10-21 15:47:38 +0000

    update the code to follow betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,19 +16,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
 
 	value_copy = strdup(value);
-
 	if (value_copy == NULL)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -39,24 +33,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	new = malloc(sizeof(hash_node_t));
-
 	if (new == NULL)
 	{
 		free(value_copy);
 		return (0);
 	}
 	new->key = strdup(key);
-	
 	if (new->key == NULL)
 	{
 		free(new);
 		return (0);
 	}
-
 	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
-
 	return (1);
 }
 
